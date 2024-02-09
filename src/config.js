3e5d07c03db1fe0a48e53cc1792cb91b8319f2f5
Thiago <thiagoil2006@gmail.com>
2024-02-09 15:44:08 -0300

    testes import

---
+++ src/config.js
@@ -0,0 +1,12 @@
+export const PORT = 3000
+
+const HOST = 'http://localhost'
+
+export const var1 = 'o Povo n quer'
+export const var2 = 'PICANHA'
+const var3 = 'o Povo quer'
+const var4 = 'SURFARRR'
+
+const teste = 'TESTE DEFAULT'
+
+export default {ola: 'Olá Mundo', teste, HOST, var3, var4}
--- src/server.js
+++ src/server.js
@@ -1,1 +1,23 @@
-console.log("Hello World")
+import express from 'express'
+import CONFIG, {PORT, var1, var2} from './config.js'
+const app = express()
+
+console.log(CONFIG.ola)
+console.log(`${var1} ${var2} ${CONFIG.var3} ${CONFIG.var4}`)
+
+app.get('/', (req, res) => {
+  res.send('Hello World!')
+})
+
+app.post('/produto', (req, res) => {
+    res.send('Hello World POST!')
+  })
+
+app.listen(PORT, () => {
+  console.log(`Server running on ${CONFIG.HOST}:${PORT}`)
+})
+
+app.get('/LULA', (req, res) => {
+    res.send(`${var1} ${var2} ${CONFIG.var3} ${CONFIG.var4}`)
+})
+
